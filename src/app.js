d433fe02112694996a2c229dffd62bfadf5aec8e
ArthurFoucher <foucherarthur@gmail.com>
2018-11-30 17:06:54 +0100

    Removes old build files, adds nodemon run cmd

--- src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class App {
-    run() {
-        console.log('hello app');
-    }
-}
-exports.App = App;
-//# sourceMappingURL=app.js.map
--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-exports.App = app_1.App;
-const app = new App();
-//# sourceMappingURL=index.js.map
