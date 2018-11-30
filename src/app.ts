6166ddbf2a040592752d6b4571ce8a6e24cc7621
ArthurFoucher <foucherarthur@gmail.com>
2018-11-30 18:18:33 +0100

    add artifacts, write source code to file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
+import * as fs from 'fs';
 import * as puppeteer from 'puppeteer';
 import config from './config';
 
+const { writeFile } = fs.promises;
 const { path } = config;
 
 export class App {
   public async run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://google.com');
-    await page.pdf({ path: `${path}/google.pdf` });
+    const response = await page.goto('https://google.com');
+
+    if (response !== null) {
+      const text = await response.text();
+      await writeFile(`${path}/source.html`, text);
+    }
 
     await browser.close();
   }
