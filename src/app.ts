59184967e2151e21d98cf4ce5059b730f88d71cf
ArthurFoucher <foucherarthur@gmail.com>
2018-11-30 17:47:03 +0100

    Add puppeteer and implement basic usage

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
+import * as puppeteer from 'puppeteer';
+import config from './config';
+
+const { path } = config;
+
 export class App {
-  public run() {
-    // tslint:disable-next-line:no-console
-    console.log('hello App');
+  public async run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    await page.pdf({ path: `${path}/google.pdf` });
+
+    await browser.close();
   }
 }
