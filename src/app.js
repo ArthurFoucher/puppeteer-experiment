4edd4074fdb61a3dd36f2449ccab7b62c1f81c58
ArthurFoucher <foucherarthur@gmail.com>
2018-11-30 16:44:22 +0100

    inital commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class App {
+    run() {
+        console.log('hello app');
+    }
+}
+exports.App = App;
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+export class App {
+  public run() {
+    // tslint:disable-next-line:no-console
+    console.log('hello app');
+  }
+}
